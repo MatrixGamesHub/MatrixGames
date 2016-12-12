557510fd94780bf8700ef85e027059afb9f9b35b
VonAncken <mail@tobiasstampfl.de>
2016-12-13 00:44:54 +0100

    ability to choose the games directory

--- Applications/GameConsole/GameConsole.py
+++ Applications/GameConsole/GameConsole.py
@@ -14,7 +14,34 @@ class GameConsoleApp(wx.App):
     def OnInit(self):
         ImageProvider.Init()
 
-        gameLoader = mtx.GameLoader('./Games', '../../Games')
+        gameLoader = None
+
+        pathList = ['./Games', '../../Games']
+        pathList = []
+        for path in pathList:
+            if os.path.isdir(path):
+                gameLoader = mtx.GameLoader(path)
+                break
+        else:
+            with wx.MessageDialog(None,
+                                  message='No games found. Please select the directory with the '
+                                          'matrix games.',
+                                  caption='Select the games directory',
+                                  style=wx.OK_DEFAULT | wx.CANCEL) as dlg:
+                dlg.OKLabel = 'Select'
+                if dlg.ShowModal() == wx.ID_CANCEL:
+                    return True
+
+            with wx.DirDialog(None, "Select the matrix games directory:",
+                              defaultPath=os.path.dirname(os.path.realpath(__file__)),
+                              style=wx.DD_DEFAULT_STYLE
+                                   | wx.DD_DIR_MUST_EXIST) as dlg:
+                dlg.CenterOnScreen()
+                if dlg.ShowModal() == wx.ID_CANCEL:
+                    return True
+
+                gameLoader = mtx.GameLoader(dlg.GetPath())
+
         gameConsole = mtx.GameConsole()
 
         ctrlHandler = mtxNet.ControllerHandler(gameConsole, gameLoader)
